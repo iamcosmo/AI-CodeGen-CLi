58f47fe85b83967d317df32623142716ebadc0f5
Chandan <hyeiamcosmos@gmail.com>
2025-04-02 20:18:08 +0530

    Setting up project

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AI-CodeGen-CLi",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "google-generativeai",
+        "python-dotenv",
+        "rich"
+    ],
+    entry_points={
+        "console_scripts": [
+            "codegen=codegen.codegen:main"
+        ]
+    },
+    author="Chandan(Cosmos)",
+    description="A CLI AI-powered Code Generator using Gemini API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamcosmo/AI-CodeGen-CLi.git",
+)
